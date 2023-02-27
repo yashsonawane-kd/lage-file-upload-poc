50511c7dc325fad8dc0907eed8875fe535ddc166
yashsonawane-kd <yash.sonawane@kickdrumtech.com>
2023-02-27 13:44:45 +0530

    WIP

--- src/components/file-uploader.tsx
+++ src/components/file-uploader.tsx
@@ -1,24 +1,24 @@
 import React, { ChangeEvent, useState } from 'react';
 import Button from '@mui/material/Button';
 import Input from '@mui/material/Input';
-import AWS from "aws-sdk";
+import AWS, { S3 } from "aws-sdk";
 import axios from 'axios';
+import { getPreSignedUrl, getPreSignedUrls } from './presigned-urls-stub';
 
 
 const FileUploader: React.FC = () => {
 
+  // file to upload
   const [selectedFile, setSelectedFile] = useState<File | undefined>(undefined);
 
-  const S3_BUCKET: string = "large-file-upload-poc1";
-  // const bucketUrl: string = process.env.REACT_APP_S3_URL ?? '';
+  //constants
+  const S3_BUCKET: string = process.env.S3_BUCKET || '';
   const accessKeyId: string | undefined = process.env.REACT_APP_ACCESS_KEY_ID || '';
   const secretAccessKey: string | undefined = process.env.REACT_APP_SECRET_ACCESS_KEY || '';
   const sessionToken: string | undefined = process.env.REACT_APP_SESSION_TOKEN || '';
 
-  if(!(accessKeyId || secretAccessKey || sessionToken)) {
-    console.log("Could not fetch AWS creds");
-  }
 
+  // S3 object for interacting with S3
   const s3: AWS.S3 = new AWS.S3({
     credentials: new AWS.Credentials(accessKeyId, secretAccessKey, sessionToken),
     signatureVersion: 'v4'
@@ -29,58 +29,13 @@ const FileUploader: React.FC = () => {
   // Perform the logic to determine file size
   // Perform the logic to upload file to AWS S3
   // To consider upload interruptions and other scenarios
-  
-  const getPreSignedUrl = (filename: string): string | null => {
-    console.log(filename);
-    try {
-      return s3.getSignedUrl('putObject', {
-        Bucket: S3_BUCKET,
-        Key: filename,
-        Expires: 120000
-      });
-    } catch(error: Error | any) {
-      console.log("Error while getting pre-signed urls");
-      console.log(error);
-      return null;
-    }
-  }
-
-  const getPreSignedUrls = async (s3: AWS.S3, uploadId: string, chunks: number, filename: string) => {
-    const baseParams = {
-      Bucket: S3_BUCKET,
-      Key: filename,
-      UploadId: uploadId
-    };
-  
-    const promises: Promise<string>[] = [];
-  
-    for (let index = 0; index < chunks; index++) {
-      promises.push(
-        s3.getSignedUrlPromise('uploadPart', {
-        ...baseParams,
-        PartNumber: index + 1
-      }));
-    }
-  
-    const res = await Promise.all(promises);
-  
-    return res.reduce((map, chunk, index) => {
-      map[index] = chunk;
-      return map
-    }, {} as Record<number, string>);
-  }
-
-  const handleFileSelect = (event: ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files?.[0];
-    setSelectedFile(file);
-  };
 
   const uploadSmallFile = async () => {
     if(!selectedFile) {
       return;
     }
 
-    const presignedUrl: string | null = await getPreSignedUrl(selectedFile.name);
+    const presignedUrl: string | null = await getPreSignedUrl(s3, selectedFile.name);
 
     if(!presignedUrl) {
       console.log("File upload failed");
@@ -96,6 +51,24 @@ const FileUploader: React.FC = () => {
       console.log(error);
     }
   }
+
+  const uploadLargeFile = async (file: File) => {
+    const multipartUpload = await s3.createMultipartUpload({Bucket: S3_BUCKET, Key: file.name}).promise();
+
+    const uploadId: string | undefined = multipartUpload.UploadId;
+
+    if(!uploadId) {
+      console.log("Error starting multipart upload");
+      return '';
+    }
+
+    return uploadId;
+  }
+
+  const handleFileSelect = (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    setSelectedFile(file);
+  };
 
   const handleUpload = async () => {
     console.log("Handling upload");
---
+++ src/components/presigned-urls-stub.ts
@@ -0,0 +1,40 @@
+export const getPreSignedUrls = async (s3: AWS.S3, uploadId: string, chunksCount: number, filename: string): Promise<Map<number, string>> => {
+    const baseParams = {
+        Bucket: process.env.S3_BUCKET || '',
+      Key: filename,
+      UploadId: uploadId
+    };
+  
+    const promises: Promise<string>[] = [];
+  
+    for (let index = 0; index < chunksCount; index++) {
+      promises.push(
+        s3.getSignedUrlPromise('uploadPart', {
+        ...baseParams,
+        PartNumber: index + 1
+      }));
+    }
+  
+    const res = await Promise.all(promises);
+  
+    return res.reduce((map: Map<number, string>, chunk, index) => {
+      map.set(index, chunk);
+      return map
+    }, {} as Map<number, string>);
+  }
+
+export const getPreSignedUrl = (s3: AWS.S3, filename: string): string | null => {
+    console.log(filename);
+    try {
+      return s3.getSignedUrl('putObject', {
+        Bucket: process.env.S3_BUCKET,
+        Key: filename,
+        Expires: 120000
+      });
+    } catch(error: Error | any) {
+      console.log("Error while getting pre-signed urls");
+      console.log(error);
+      return null;
+    }
+  }
+
