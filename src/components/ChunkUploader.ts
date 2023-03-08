1d02c2b012fed8a7e2b3f803f06d0173fa859287
yashsonawane-kd <yash.sonawane@kickdrumtech.com>
2023-03-08 12:11:20 +0530

    moved uploaders into separate files

---
+++ src/components/ChunkUploader.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+export class ChunkUploader {
+  presignedUrl: string;
+  etag: string;
+  index: number;
+  start: number;
+  end: number;
+  file: File;
+  completed: boolean;
+  retryInterval: number = 1_000;
+
+  constructor(
+    presignedUrl: string,
+    index: number,
+    start: number,
+    end: number,
+    file: File
+  ) {
+    this.presignedUrl = presignedUrl;
+    this.etag = "";
+    this.index = index;
+    this.start = start;
+    this.end = end;
+    this.file = file;
+    this.completed = false;
+  }
+
+  async upload(
+    success: CallableFunction,
+    failure: CallableFunction
+  ): Promise<ChunkUploader> {
+    const fileChunk: Blob = this.file.slice(this.start, this.end);
+    // console.log("Uploading chunk ", this.index, " of size ", fileChunk.size);
+
+    try {
+      const response = await axios.put(this.presignedUrl, fileChunk);
+      this.etag = response.headers.etag;
+      this.completed = true;
+      success(this.index, this.etag);
+    } catch (error: Error | unknown) {
+      this.completed = false;
+      console.log("Upload failed for chunk: ", this.index);
+      failure(this);
+    }
+
+    return this;
+  }
+}
